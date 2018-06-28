03ec6f5643ac3f4b00dc51d14530b0e0a719c45d
Eddie Choi <fdsa0329@naver.com>
2018-06-29 00:17:35 +0900

    asdasd

---
+++ BookExercises/calculate two different list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct  poly {
+  int value;
+} poly;
+
+void addingpoly(int* a, int* b){
+  int i;
+  for(i = 0; i > 3; ++i){
+
+
+  }
+}
+
+void subtractingpoly(){
+
+}
+
+int main(){
+  int a[3] = {1, 2, 3};
+  int b[3] = {3, 4, 5};
+
+
+}
