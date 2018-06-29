22c38efc024e3aa7e480ce6eb6cec728474836e9
Eddie Choi <fdsa0329@naver.com>
2018-06-29 20:47:09 +0900

    qweqw

---
+++ BookExercises/creating node.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <time.h>
+
+typedef struct Node {
+  int data;
+  int* next;
+  typedef struct* head;
+} node;
+
+void create_node(int data, int* next) {
+  int* head = node.head;
+  head = (int *)malloc(sizeof(int*));
+  head -> node.next;
+
+
+
+}
+
+
+
+int main() {
+
+  return 0;
+}
--- BookExercises/rear_table.c
+++ BookExercises/rear_table.c
@@ -3,31 +3,16 @@
 #include <malloc.h>
 #include <time.h>
 
-
+typedef struct element {
+  int row;
+  int col;
+  int value;
+} element;
 
 int main() {
   int a[3][3] = {{2, 3, 0,},
                  {8, 9, 1,},
                  {7, 0, 5}};
-  int col;
-  int row;
-  int b[3][7];
-
-  for(col = 0; col < 3; ++col) {
-      for(row = 0; row < 3; ++row) {
-        if(a[col][row] == 0) {
-          
-        }
-        else {
-
-        }
-      }
-
-
-
-
-  }
-
 
 
   // int b[6][6] = {{0, 0, 0, 7, 0, 0},
